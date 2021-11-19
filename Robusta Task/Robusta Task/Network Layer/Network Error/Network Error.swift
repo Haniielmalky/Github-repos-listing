5781f89daa6caf4789df36c61cb970380429d0ec
haniielmalky <haniielmalky@gmail.com>
2021-11-19 20:18:45 +0200

    Project Initation
    - Create the project
    - Create the network layer: API Request, Image Loader

---
+++ Robusta Task/Robusta Task/Network Layer/Network Error/Network Error.swift
@@ -0,0 +1,38 @@
+//
+//  APIErrorManager.swift
+//  Robusta Task
+//
+//  Created by Hani El-Malky on 19/11/2021.
+//
+
+import Foundation
+
+// MARK: - Network Errors
+
+enum NetworkError: Error {
+    case invalidRequest
+    case response
+    case statusCode(Int)
+    case data
+}
+
+class  NetworkErrorManager {
+    
+    static let shared = NetworkErrorManager()
+    
+    func checkForErrors(in response: URLResponse?, error: Error?) -> Error? {
+        if let error = error {
+            return error
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            return NetworkError.response
+        }
+        
+        if (200..<300).contains(httpResponse.statusCode) {
+            return NetworkError.statusCode(httpResponse.statusCode)
+        }
+        
+        return nil
+    }
+}
---
+++ Robusta Task/Robusta Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Robusta Task
+//
+//  Created by Hani El-Malky on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
